051759c199e201078d96ffb292d6352cb01b8d19
TranChiHuynh200065 <huynhtcgcc200065@fpt.edu.vn>
2022-09-24 10:23:34 +0700

    Huynh

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	
 	mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);*/
-	$Connect = pg_connect("postgres://zlahqttpvenofp:21d03f95e68c9eed4a9caf13b572719e8d729527b2b482df8d2eaabd812bb6ff@ec2-3-209-124-113.compute-1.amazonaws.com:5432/deb9i4dev18mvb");
+	$Connect = pg_connect("postgres://jmdrhuyxeteupz:59d17582c5cf3f93d4c70701169649492bf1ca1c26e7be4c1fed36ee1c91e358@ec2-34-205-14-168.compute-1.amazonaws.com:5432/d2hk0ut4ifpeo7");
     //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
 	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
 	
